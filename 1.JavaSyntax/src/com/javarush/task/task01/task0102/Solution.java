c35fb94102616b528bb2030e0c459b8f028e75a4
Sergey Butchak <Sergei.Butchak@gmail.com>
2021-09-05 23:10:51 +0400

    Syntax Level 0 completed

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0102/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0102;
+
+/* 
+Комплимент учителю
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Элли самая красивая\nЭлли самая красивая\nЭлли самая красивая\nЭлли самая красивая\n" +
+                "Элли самая красивая");
+    }
+}
