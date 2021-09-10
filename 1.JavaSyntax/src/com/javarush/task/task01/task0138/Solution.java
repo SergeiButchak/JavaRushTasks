1c76fbcdcc217650ee4924ba312c91e552ba9e28
Sergey Butchak <Sergei.Butchak@gmail.com>
2021-09-10 18:05:48 +0400

    Syntax Level 1 completed

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println("\n" +
+                "Я помню чудное мнговенье,\n" +
+                "Передо мной явилась ты,\n" +
+                "Как мимолетное виденье\n" +
+                "Как гений чистой красоты.");
+    }
+}
