5f63e5ccea84085d9b6ab5407be49243d400b3c7
Sergey Butchak <Sergei.Butchak@gmail.com>
2021-09-12 11:12:36 +0400

    Syntax Level 2 completed

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0208/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task02.task0208;
+
+/* 
+Одного кота нам мало
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat murzik = new Cat();
+        Cat murka = new Cat();
+    }
+
+    public static class Cat {
+
+    }
+}
